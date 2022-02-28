52ffa49ab08f087036178c276f6944cf7cc3fee9
Katherine Forester <forester.katherine.m@gmail.com>
2022-02-27 18:36:13 -0600

    create all variables. first pass at password function.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,97 +1,47 @@
-// Assignment code here
+//variables
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var lower = 'abcdefjhijklmnopqrstuvwxyz'
 var num = '0123456789'
+// start with an empty character variable, add to it later
 var char = ''
-//generate a random password length
+//generate a random password length between 8 & 25
 var length = Math.floor((Math.random() * 17) + 8);
+//do you want to include upper?
 var promptUpper = window.prompt('Would you like your password to include UPPERCASE letters? Enter "yes" or "no"');
+//do you want to include lower?
+var promptLower = window.prompt('Would you like your password to include lowercase letters? Enter "yes" or "no"');
+//do you want to include numbers?
+var promptNumber = window.prompt('Would you like your password to include numbers? Enter "yes" or "no"');
 
-
-
+//generate the password
 var generatePassword = function () {
-  //ask user if they'd like to include uppercase
-  var promptUpper = window.prompt('Would you like your password to include UPPERCASE letters? Enter "yes" or "no"');
 
   promptUpper = promptUpper.toLowerCase();
+  promptLower = promptLower.toLowerCase();
+  promptNumber = promptNumber.toLowerCase();
 
-  //if yes, add uppercase letters to char variable
-  if (promptUpper === 'yes') {
-    char = char + upper;
-  } else if (promptUpper === 'no') {
-    //if not, don't add uppercase letters
-    char = char
-  } 
-  //anything other than yes or no, return error
-  else {
-    window.alert("You did not make a valid selection.")
+  if ((promptUpper === 'yes') && (promptLower === 'yes') && (promptNumber === 'yes')) {
+    char = (upper + lower + num)
+  } else if ((promptUpper === 'yes') && (promptLower === 'yes')) {
+    char = (upper + lower)
+  } else if ((promptUpper === 'yes') && (promptNumber === 'yes')) {
+    char = (upper + num)
+  } else if ((promptLower === 'yes') && (promptNumber === 'yes')) {
+    char = (lower + num)
+  } else if (promptUpper === 'yes') {
+    char = upper
+  } else if (promptLower === 'yes') {
+    char = lower
+  } else if (promptNumber === 'yes') {
+    var char = num
   }
-  return selector();
+  //what to do with it
+  var pword = '';
+  for (var i = 0; i < length; i++) {
+    pword += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return pword;
 };
-
-
-  // var promptLower = window.prompt('Would you like your password to include lowercase letters? Enter "yes" or "no"');
-  // switch(promptLower.toLowerCase()) {
-  //   case 'yes':
-  //     char = char + lower
-  //     break;
-  //   case 'no':
-  //     console.log(false);
-  //   break;
-  //   default:
-  //     console.error("You did not make a valid selection.")
-  //     return promptLower();
-  // }
-  // var promptNumber = window.prompt('Would you like your password to include numbers? Enter "yes" or "no"');
-  // switch(promptNumber.toLowerCase()) {
-  //   case 'yes':
-  //     char = char + num
-  //     break;
-  //   case 'no':
-  //     console.log(false);
-  //   break;
-  //   default:
-  //     console.error("You did not make a valid selection.")
-  //     return promptNumber();
-  // }
-  // var lenString = window.prompt("How many characters should your password be? Enter number between 8 and 28");
-  // const len = Number.parseInt(lenString);
-  // switch(len) {
-  //   case 'yes':
-  //     char = char + num
-  //     break;
-  //   case 'no':
-  //     console.log(false);
-  //   break;
-  //   default:
-  //     alert("You did not make a valid selection.")
-  //     return promptNumber();
-  // }
-
-//   if ((useUpper) && (useLower) && (useNum)) {
-//       var char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-//   } else if ((useUpper) && (useLower)) {
-//     var char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-//   } else if ((useUpper) && (useNum)) {
-//     var char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-//   } else if ((useLower) && (useNum)) {
-//     var char = 'abcdefghijklmnopqrstuvwxyz0123456789'
-//   } else if (useUpper) {
-//     var char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-//   } else if (useLower) {
-//     var char = 'abcdefghijklmnopqrstuvwxyz'
-//   } else if (useNum) {
-//       var char = '0123456789'
-//   } else {window.alert('You need to provide a valid answer! Please try again.');
-//   return promptNum();
-//   };
-
-// // var generatePassword = function () {
-// //   for (var I=0; i<password_length; i++) {
-// //       var rnum = Math.floor(Math.random() * char.length);
-// //       randomstring += char.substring(rnum,rnum+1);
-// //   };
-// // };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -108,4 +58,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-selector();
+generatePassword();
